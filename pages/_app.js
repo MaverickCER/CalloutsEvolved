37c3e0c393e04dfc9c5ea90141b36163c87f16e1
Maverick Manasco <MaverickCER@gmail.com>
2022-03-29 14:14:07 -0400

    CE-3 #comment Initialize analytics and hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
-import Layout from "../components/Layout";
-import { AuthProvider } from "../contexts/AuthContext";
-import "../styles/globals.css";
+import { useEffect } from 'react';
+import Layout from '../components/Layout';
+import { AuthProvider } from '../contexts/AuthContext';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-123456789', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <AuthProvider>
       <Layout>
