3c76cfa9a6bdd715a7e0f18ab7806ead6ade5e36
Maverick Manasco <MaverickCER@gmail.com>
2022-03-29 21:46:48 -0400

    CEP update

--- utils/csp.js
+++ utils/csp.js
@@ -1,22 +1,22 @@
-import crypto from "crypto";
+import crypto from 'crypto';
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 const getCsp = (inlineScriptSource) => {
   const csp = [];
-  const hash = crypto.createHash("sha256").update(inlineScriptSource);
+  const hash = crypto.createHash('sha256').update(inlineScriptSource);
 
   csp.push(`base-uri 'self'`);
   csp.push(`form-action 'self'`);
   csp.push(`default-src 'self'`);
   csp.push(
-    `script-src 'self'${
-      isProd ? "" : ` 'unsafe-eval'`
-    }  https://*.firebaseio.com https://cdn.firebase.com https://js.stripe.com 'sha256-${hash.digest(
-      "base64"
+    `script-src 'self https:' //*.firebaseio.com https://cdn.firebase.com https://js.stripe.com https://ajax.googleapis.com/ajax/ https://cdnjs.cloudflare.com/ajax/ https://activity-project-unit.herokuapp.com'sha256-${hash.digest(
+      'base64'
     )}'`
   );
-  csp.push(`style-src 'self'  'unsafe-inline'`);
+  csp.push(
+    `style-src 'self'  'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/ https://cdnjs.cloudflare.com/ajax/`
+  );
   csp.push(
     `connect-src 'self' vitals.vercel-insights.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com wss://*.firebaseio.com   https://firestore.googleapis.com`
   );
@@ -25,7 +25,7 @@ const getCsp = (inlineScriptSource) => {
   csp.push(`frame-src *`);
   csp.push(`media-src *`);
 
-  return csp.join("; ");
+  return csp.join('; ');
 };
 
 module.exports = {
