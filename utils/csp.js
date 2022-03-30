cca1d19cb28ec7f7215e70230c565f001f5622a8
Maverick Manasco <MaverickCER@gmail.com>
2022-03-30 09:46:01 -0400

    CE-4 #comment CSP Update

--- utils/csp.js
+++ utils/csp.js
@@ -7,23 +7,21 @@ const getCsp = (inlineScriptSource) => {
   const hash = crypto.createHash('sha256').update(inlineScriptSource);
 
   csp.push(`base-uri 'self'`);
-  csp.push(`form-action 'self'`);
-  csp.push(`default-src 'self'`);
   csp.push(
-    `script-src "script-src 'self https:'*.firebaseio.com https://cdn.firebase.com https://js.stripe.com https://ajax.googleapis.com/ajax/ https://cdnjs.cloudflare.com/ajax/ https://activity-project-unit.herokuapp.com'sha256-${hash.digest(
+    `connect-src 'self'  https://www.google-analytics.com https://firebase.googleapis.com/v1alpha/projects/-/apps/1:580878402383:web:e352bf5ae81444a58f3b3a/webConfig vitals.vercel-insights.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com wss://*.firebaseio.com   https://firestore.googleapis.com`
+  );
+  csp.push(`default-src 'self'`);
+  csp.push(`font-src 'self' data:`);
+  csp.push(`form-action 'self'`);
+  csp.push(`frame-src *`);
+  csp.push(`img-src * data: blob:`);
+  csp.push(`media-src *`);
+  csp.push(
+    `script-src 'self' 'unsafe-inline' 'unsafe-eval' http://*.hotjar.com https://*.hotjar.com https://www.googletagmanager.com https://www.google-analytics.com https://*.firebaseio.com https://cdn.firebase.com https://js.stripe.com 'sha256-${hash.digest(
       'base64'
     )}'`
   );
-  csp.push(
-    `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/ https://cdnjs.cloudflare.com/ajax/`
-  );
-  csp.push(
-    `connect-src 'self' vitals.vercel-insights.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com wss://*.firebaseio.com   https://firestore.googleapis.com`
-  );
-  csp.push(`img-src * data: blob:`);
-  csp.push(`font-src 'self' data:`);
-  csp.push(`frame-src *`);
-  csp.push(`media-src *`);
+  csp.push(`style-src 'self' 'unsafe-inline'`);
 
   return csp.join('; ');
 };
