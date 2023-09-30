b0b4970f58681fef401ca7ec1584223ba078d2f5
Maverick Manasco <MaverickCER@gmail.com>
2023-09-30 03:08:35 -0400

    agc concept

--- components/post-body.jsx
+++
@@ -1,9 +0,0 @@
-import markdownStyles from '../styles/markdown-styles.module.css';
-
-export default function PostBody({ content }) {
-  return (
-    <div className="max-w-2xl mx-auto" style={{ width: '90vw', maxWidth: '800px', margin: 'auto' }}>
-      <div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  );
-}
--- pages/404.js
+++
@@ -1,5 +0,0 @@
-const Custom404 = () => {
-  return <h1>404 - Page not Found</h1>;
-};
-
-export default Custom404;
